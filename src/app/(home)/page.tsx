3983018aa3ce159a7dfb3b4a524e9b99d8cf6a21
Vishal7906 <viratrmr@gmail.com>
2024-07-11 18:18:31 +0530

    reorganize components

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,5 +1,4 @@
-import Image from "next/image";
-import Banner from "@/components/Banner";
+import Banner from "./components/Banner";
 
 export default function Home() {
   return (
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -19,7 +19,7 @@ const Navbar = () => {
             </div>
             <div className='flex items-center gap-3'>
                 <button className="h-10 rounded-md border border-primary-500 px-4 py-2 text-sm font-medium text-primary-500 transition-all hover:border-primary-100 hover:bg-primary-100 active:border-primary-200 active:bg-primary-200">Sign In</button>
-                <button className="h-10 rounded-md border border-primary-500 px-4 py-2 text-sm font-medium text-primary-500 transition-all hover:border-primary-100 hover:bg-primary-100 active:border-primary-200 active:bg-primary-200">Sign Up</button>
+                <button className="h-10 rounded-md border border-primary-500 bg-primary-500 px-4 py-2 text-sm font-medium text-white transition-all hover:bg-primary-600 active:bg-primary-700">Sign Up</button>
             </div>
         </div>
       </nav>
